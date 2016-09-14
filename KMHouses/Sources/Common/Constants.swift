c0d15aab1c770dce463a18f51039c47bcb512239
Pankaj Rathor <pankajrathor@Pankajs-MacBook-Pro.local>
2016-09-14 14:01:01 +0200

    Initial project commit

---
+++ KMHouses/Sources/Common/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  KMHouses
+//
+//  Created by Pankaj Rathor on 14/09/16.
+//  Copyright © 2016 Sogeti B.V. All rights reserved.
+//
+
+import Foundation
---
+++ KMHouses/Sources/Model/House.swift
@@ -0,0 +1,13 @@
+//
+//  House.swift
+//  KMHouses
+//
+//  Created by Pankaj Rathor on 14/09/16.
+//  Copyright © 2016 Sogeti B.V. All rights reserved.
+//
+
+import Cocoa
+
+class House: NSObject {
+
+}
